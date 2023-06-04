e2dbef8a713c9441ead97937cbb4470885e60c48
Madhav007 <madhavaprashath669@gmail.com>
2023-06-04 17:34:58 +0530

    Added all stories of a particular user feature

--- routes/stories.js
+++ routes/stories.js
@@ -57,6 +57,25 @@ router.get('/:id', ensureAuth, async (req, res) => {
     }
 })
 
+// GET all stories of a particular user
+router.get('/user/:userId', ensureAuth, async(req, res) => {
+    try{
+        const stories = await Story.find({
+            user : req.params.userId,
+            status: 'public'
+        })
+        .populate('user')
+        .lean()
+
+        res.render('stories/index',{
+            stories
+        })
+    }catch(err){
+        console.log(err)
+        res.render('error/500')
+    }
+})
+
 // Get the form page for editing the story. GET method.
 router.get('/edit/:id', ensureAuth, async (req, res) => {
 
