3773154b47429b58780e0d51aa5c5b1eb6ee9b17
Madhav007 <madhavaprashath669@gmail.com>
2023-06-05 16:36:26 +0530

    test change

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ module.exports = function(passport) {
    passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback"
+        callbackURL: "https://storywise.onrender.com/auth/google/callback"
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
